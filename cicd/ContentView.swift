817b839424c8f99ec3137c2753a1ed067a038b5e
liangwangcm <liang.wang@covermore.com>
2022-05-16 15:49:51 +1000

    first commit

---
+++ cicd/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  cicd
+//
+//  Created by liang.wang on 16/5/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ cicd/cicdApp.swift
@@ -0,0 +1,17 @@
+//
+//  cicdApp.swift
+//  cicd
+//
+//  Created by liang.wang on 16/5/2022.
+//
+
+import SwiftUI
+
+@main
+struct cicdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
